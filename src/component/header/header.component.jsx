7a092a98782f1608ce7bc75962dfe29b832a9f3f
Swanand Mehta <Swanand.Mehta@kpit.com>
2021-01-13 11:02:51 +0530

    Header setup completed

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,5 +1,21 @@
+import './header.style.scss';
+import { ReactComponent as Logo } from '../../img/logo.svg';
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
     return (
-        <header>I am header</header>
+        <header className="header">
+            <div className="logo-container">
+                <Link className="header-menu" to="/">
+                    <Logo className='logo' />
+                </Link>
+            </div>
+            <div className="menu-container">
+                <Link className="header-menu" to="/shop">SHOP</Link>
+                <Link className="header-menu" to="/contact">CONTACT</Link>
+                <Link className="header-menu" to="/sign-up">SIGN IN</Link>
+                <Link className="header-menu" to="/cart">CART</Link>
+            </div>
+        </header>
     )
 }
